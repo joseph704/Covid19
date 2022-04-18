653668b2a06c27a5101114459a2a01eb03868236
차요셉 <jscha@raoncorp.com>
2022-04-18 20:07:23 +0900

    시도별 코로나 데이터 파싱 로직 추가

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -15,5 +16,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 순환 참조 방지
+        self.fetchCovidOverView(completionHandler: { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success(value):
+                debugPrint("success \(value)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        })
+    }
+    
+    func fetchCovidOverView (
+        completionHandler: @escaping (Result<CityCovidOverivew, Error>) -> Void
+    ) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "RgZpzPyid1DOoI7B6L5NXf94UGSqwu2cx"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverivew.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
     }
 }
